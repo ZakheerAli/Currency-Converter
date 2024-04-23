8ae39de63b544c17897a4bedfd399452ae9c088d
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-23 22:36:47 +0500

    Typescript Project

---
+++ index.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+let c_value:{[key:string]:number}={
+    KWD:0.31,
+    BHD:0.38,
+    OMR:0.38,
+    EURO:0.93,
+    USD:1,
+}
+let ans=await inquirer.prompt([
+    {
+        type:"list",
+        name:"from",
+        message:"convert from",
+        choices:["KWD","BHD","OMR","EURO","USD"]
+    },
+    {
+        type:"list",
+        name:"to",
+        message:"convert in to ",
+        choices:["KWD","BHD","OMR","EURO","USD"] 
+    },
+    {
+        type:"number",
+        name:"amount",
+        message:"Your amount is "
+    }
+])
+let result=(c_value[ans.to]/c_value[ans.from]*ans.amount);
+console.log(result);
